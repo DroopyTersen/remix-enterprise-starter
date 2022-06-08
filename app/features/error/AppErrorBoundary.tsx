87105b13b1548d25421ac71e1b7497d10a7cb387
Jeremiah Billmann <jbillmann@gmail.com>
2022-06-08 08:43:08 -0600

    Upgrade AppErrorBoundary

--- app/features/error/AppErrorBoundary.tsx
+++ app/features/error/AppErrorBoundary.tsx
@@ -1,9 +1,23 @@
 import { ErrorContainer } from "~/ui-toolkit/components/ErrorContainer/ErrorContainer";
 
 export const AppErrorBoundary = ({ error }) => {
+  let message = "Unknown error";
+
+  // It's a string
+  if (typeof error === "string") message = error;
+  // It's a normal error
+  else if (error?.message) message = error?.message;
+  // It's an array of errors
+  else if (Array.isArray(error)) {
+    message = error
+      ?.map((e) => e?.message)
+      .filter(Boolean)
+      .join(", ");
+  }
+
   return (
     <ErrorContainer>
-      <pre>{error?.message}</pre>
+      <pre>{message}</pre>
     </ErrorContainer>
   );
 };
