45c7e1e04f69e116fb9734628247310ed0985a00
Andrew Petersen <andrew.petersen15@gmail.com>
2022-06-09 12:29:43 -0600

    Moved dotenv.config() to entry.server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,13 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
+import dotenv from "dotenv";
+import * as path from "node:path";
 import { renderToString } from "react-dom/server";
+
+console.log("🚀 | __dirname", __dirname);
+let envPath = path.join(__dirname, "../env", ".env." + (process.env.PUBLIC_ENV || "local"));
+console.log("🚀 | envPath", envPath);
+dotenv.config({ path: envPath });
 
 export default function handleRequest(
   request: Request,
--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,3 @@
-const path = require("path");
-let envPath = path.join(__dirname, "env", ".env." + (process.env.PUBLIC_ENV || "local"));
-
-require("dotenv").config({ path: envPath });
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
