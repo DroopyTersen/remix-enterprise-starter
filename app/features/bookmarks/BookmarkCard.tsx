fcf6e980c943bfc5a393c16e22198e7c3a8a7eaf
Andrew Petersen <andrew.petersen15@gmail.com>
2022-06-09 09:54:59 -0600

    removed react-router-dom ref

--- app/features/bookmarks/BookmarkCard.tsx
+++ app/features/bookmarks/BookmarkCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "@remix-run/react";
 import { FormButton } from "~/ui-toolkit/components/Button/FormButton";
 import Card from "~/ui-toolkit/components/Card/Card";
 import { Bookmark } from "./bookmark.types";
