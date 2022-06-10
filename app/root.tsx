a0394bf93f3cdc046bc97d7a4a420ef4e302ea2f
Andrew Petersen <andrew.petersen15@gmail.com>
2022-06-10 13:07:23 -0600

    added root catch boundary for 404s

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,16 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch,
+} from "@remix-run/react";
 import { AppLayout } from "./features/layout/AppLayout";
 import globalStyles from "./global.css";
+import { ErrorContainer } from "./ui-toolkit/components/ErrorContainer/ErrorContainer";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -54,3 +63,31 @@ const getPublicEnvVars = () => {
   }, {});
   return publicEnv;
 };
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  let title = caught?.status === 404 ? "There's nothing here!" : "Error";
+  let message =
+    caught?.status === 404
+      ? "The page your looking for doesn't exist."
+      : "Uh oh. Something went wrong.";
+  return (
+    <html className="h-100">
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-100">
+        <AppLayout>
+          <div className="px-4 py-5 h-100">
+            <ErrorContainer title={title}>
+              <pre>{message}</pre>
+            </ErrorContainer>
+          </div>
+        </AppLayout>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
